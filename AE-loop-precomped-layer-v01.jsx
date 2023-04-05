fb8c39139f096cdf48b63d15ac25c2ec8f76bf55
damnbill <85885856+damnbill@users.noreply.github.com>
2023-04-05 17:44:02 -0400

    Add files via upload

---
+++ AE-loop-precomped-layer-v01.jsx
@@ -0,0 +1,28 @@
+﻿// get the currently selected layer
+var selectedLayer = app.project.activeItem.selectedLayers[0];
+var curComp = app.project.activeItem;
+
+// split the duplicated layer in two
+var compDuration = app.project.activeItem.duration;
+var splitTime = compDuration / 2;
+
+// duplicate the selected layer
+var fadeoutLayer = selectedLayer.duplicate();
+var fadeinLayer = fadeoutLayer.duplicate();
+
+// set names 
+fadeoutLayer.name = "fadeout";
+fadeinLayer.name = "fadein";
+
+// move around
+curComp.layer("fadeout").startTime = -compDuration/2 ;
+curComp.layer("fadeout").inPoint = 0;
+curComp.layer("fadeout").outPoint = 1;
+curComp.layer("fadein").startTime = compDuration/2 ;
+curComp.layer("fadein").inPoint = compDuration-1;
+
+// keyframe opacity
+curComp.layer("fadeout").property("Opacity").setValueAtTime(0, 100);
+curComp.layer("fadeout").property("Opacity").setValueAtTime(1, 0);
+curComp.layer("fadein").property("Opacity").setValueAtTime(compDuration, 100);
+curComp.layer("fadein").property("Opacity").setValueAtTime(compDuration-1, 0);
